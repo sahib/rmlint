eda9ea8a1c60582b7d4da64794a9c2f9908f4eec
Chris P. <sahib@online.de>
2011-04-19 14:27:27 -0700

    Edited src/defs.h via GitHub

--- src/defs.h
+++ src/defs.h
@@ -65,7 +65,7 @@
 #define MD5_FP_MAX_RSZ     8192      /* The maximal size read in for fingerprints. Default   4K */
 #define MD5_FP_PERCENT     10 	     /* Percent of a file read in for fingerprint. Default  10% */
 #define MD5_SERIAL_IO      1         /* Align threads before doing md5 related IO. Default:   1 */
-#define MD5_USE_MMAP       1         /* Use mmap() instead of fread() EXPERIMENTAL! Use = risk! */
+#define MD5_USE_MMAP      -1         /* Use mmap() instead of fread() EXPERIMENTAL! Use = risk! */
 
 /* Do not use O_DIRECT! read() will do weird things */
 /*
