126fc70e982ce202ed2053f9c99a8e9d81b1fd69
Chris P <sahib@online.de>
2012-05-30 19:38:16 +0200

    Added missing files (thanks to Josh VanderLinden

---
+++ src/useridcheck.h
@@ -0,0 +1,32 @@
+/**
+ *  This file is part of rmlint.
+ *
+ *  rmlint is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  rmlint is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with rmlint.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ ** Author: Christopher Pahl <sahib@online.de>:
+ ** Hosted on http://github.com/sahib/rmlint
+ *
+ **/
+
+#include <stdbool.h>
+
+typedef struct
+{
+    unsigned long gid;
+    unsigned long uid;
+} UserGroupList;
+
+UserGroupList ** userlist_new(void);
+bool userlist_contains(UserGroupList ** list, unsigned long uid, unsigned gid, bool * valid_uid, bool * valid_gid);
+void userlist_destroy(UserGroupList ** list);
