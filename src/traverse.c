05e67c658eda6ac341701d77e55fffb13d568751
SeeSpotRun <thomas_d_j@yahoo.com.au>
2014-06-24 01:07:20 +1000

    Initial implementation of fts traversal of user-input paths (replaces ntfw).

---
+++ src/traverse.c
@@ -0,0 +1,217 @@
+/**
+ *  This file is part of rmlint.
+ *
+ *  rmlint is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  rmlint is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with rmlint.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ ** Author: Christopher Pahl <sahib@online.de>:
+ ** Hosted on http://github.com/sahib/rmlint
+ *
+ **/
+#include <errno.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <fts.h>
+#include <sys/stat.h>
+#include "list.h"
+#include "rmlint.h"
+#include "filter.h"
+#include "linttests.h"
+
+
+static int process_file (FTSENT *ent, bool is_ppath, int pnum, int lint_type) {
+    /*TODO: regex check if filename exclude*/
+    if (!lint_type) {
+        /*see if we can find a lint type*/
+        if (junkinbasename(ent->fts_path, set))
+            lint_type=TYPE_JNK_FILENAME;
+        else if ((lint_type = uid_gid_check(ent, set))) {}
+        else if (is_old_tmp(ent, set)) {
+            lint_type = TYPE_OTMP;
+        } else if(is_nonstripped(ent, set)) {
+            lint_type = TYPE_NBIN;
+        }
+    }
+
+    if (!lint_type) lint_type=TYPE_DUPE_CANDIDATE;
+
+    switch (ent->fts_info) {
+    case FTS_D:         /* preorder directory */
+    case FTS_DC:        /* directory that causes cycles */
+    case FTS_DNR:       /* unreadable directory */
+    case FTS_DOT:       /* dot or dot-dot */
+    case FTS_DP:        /* postorder directory */
+    case FTS_ERR:       /* error; errno is set */
+    case FTS_INIT:      /* initialized only */
+    case FTS_SLNONE:    /* symbolic link without target */
+    case FTS_W:         /* whiteout object */
+    case FTS_NS:        /* stat(2) failed */
+    case FTS_NSOK:      /* no stat(2) requested */
+        list_append(ent->fts_path,
+                    0,
+                    ent->fts_statp->st_mtim,
+                    ent->fts_statp->st_dev,
+                    ent->fts_statp->st_ino,
+                    lint_type, is_ppath, pnum );
+        break;
+    case FTS_F:         /* regular file */
+    case FTS_SL:        /* symbolic link */
+    case FTS_DEFAULT:   /* none of the above */
+        list_append(ent->fts_path,
+                    ent->fts_statp->st_size,
+                    ent->fts_statp->st_mtim,
+                    ent->fts_statp->st_dev,
+                    ent->fts_statp->st_ino,
+                    lint_type, is_ppath, pnum );
+        break;
+    default:
+        break;
+    } /* end switch(p->fts_info)*/
+
+    return 1;
+}
+
+/* Traverse the file hierarchies named in PATHS, the last entry of which
+ * is NULL.  FTS_FLAGS controls how fts works.
+ * Return true if successful.  */
+
+int traverse_path (rmlint_settings  *settings, int  pathnum, int fts_flags) {
+    int numfiles = 0;
+    int dir_file_counter = 0;
+    char is_ppath = settings->is_ppath[pathnum];
+    char** paths=malloc(sizeof(char*)*2);
+    FTS *ftsp;
+    FTSENT *p, *chp;
+    if (settings->paths[pathnum]) {
+        /* convert into char** structure for passing to fts */
+        paths[0]=settings->paths[pathnum];
+        paths[1]=NULL;
+    } else {
+        error("Error: no paths defined for traverse_files");
+        return -1;
+    }
+
+    if ((ftsp = fts_open(paths, fts_flags, NULL)) == NULL) {
+        error("fts_open failed");
+        return -1;
+    }
+
+    /* Initialize ftsp */
+    chp = fts_children(ftsp, 0);
+    if (chp == NULL) {
+        warning("fts_children: can't initialise");
+        return 0;               /* no files to traverse */
+    }
+    while (!iAbort && (p = fts_read(ftsp)) != NULL) {
+        bool junkdirskip=false;
+        dir_file_counter++;
+        switch (p->fts_info) {
+        case FTS_D:         /* preorder directory */
+            if(junkinbasename(p->fts_path, settings)) {
+                info("Junk dir %s\n", p->fts_path);
+                process_file(p, is_ppath, pathnum, TYPE_JNK_DIRNAME);
+                junkdirskip = (settings->collide);
+            }
+            if (
+                (settings->depth!=0 && p->fts_level>=settings->depth) ||
+                /* continuing into folder would exceed maxdepth*/
+                (settings->ignore_hidden && p->fts_level > 0 && p->fts_name[0] == '.') ||
+                /* ignoring hidden folders */
+                (regfilter(paths[0], settings->dpattern)) ||
+                /* does not match regex */
+                (junkdirskip)) {
+                fts_set(ftsp,p,FTS_SKIP);
+            } else {
+                dir_file_counter=0;
+            }
+            break;
+        case FTS_DC:        /* directory that causes cycles */
+            warning(RED"Warning: filesystem loop detected between:\nskipping:\t%s\n(same as):\t%s\n"NCO,
+                    p->fts_path, p->fts_cycle->fts_path );
+            break;
+        case FTS_DNR:       /* unreadable directory */
+            warning(RED"Warning: cannot read directory %s (skipping)\n"NCO, p->fts_path);
+            break;
+        case FTS_DOT:       /* dot or dot-dot */
+            break;
+        case FTS_DP:        /* postorder directory */
+            if (dir_file_counter==0) {
+                numfiles += process_file(p, is_ppath, pathnum, TYPE_EDIR);
+            }
+            break;
+        case FTS_ERR:       /* error; errno is set */
+            warning(RED"Warning: error %d in fts_read for %s (skipping)\n"NCO, errno, p->fts_path);
+            break;
+        case FTS_INIT:      /* initialized only */
+            break;
+        case FTS_SLNONE:    /* symbolic link without target */
+            warning(RED"Warning: symlink without target: %s\n"NCO, errno, p->fts_path);
+            numfiles += process_file(p, is_ppath, pathnum, TYPE_BLNK);
+            break;
+        case FTS_W:         /* whiteout object */
+            break;
+        case FTS_NS:        /* stat(2) failed */
+            warning(RED"Warning: cannot stat file %s (skipping)\n", p->fts_path);
+        case FTS_NSOK:      /* no stat(2) requested */
+        case FTS_SL:        /* symbolic link */
+        case FTS_F:         /* regular file */
+        case FTS_DEFAULT:   /* any file type not explicitly described by one of the above*/
+            numfiles += process_file(p, is_ppath, pathnum, 0); /* this is for any of FTS_NSOK, FTS_SL, FTS_F, FTS_DEFAULT*/
+        default:
+            break;
+        } /* end switch(p->fts_info)*/
+    } /*end while ((p = fts_read(ftsp)) != NULL)*/
+
+    if (errno != 0) {
+        error ("Error %d: fts_read failed: %s", 0, errno, ftsp->fts_path);
+        numfiles = -1;
+    }
+
+    fts_close(ftsp);
+    return numfiles;
+
+    return numfiles;
+}
+
+/*--------------------------------------------------------------------*/
+/* Traverse file hierarchies based on settings contained in SETTINGS;
+ * add the files found into LIST
+ * Return file count if successful.  */
+
+int rmlint_search_tree( rmlint_settings *settings) { /*, rmlint_filelist *list)*/
+    int numfiles=0;
+    int cpindex=0;
+    /* Set Bit flags for fts options.  */
+    int bit_flags = 0;
+    if (!settings->followlinks)
+        bit_flags|=FTS_COMFOLLOW | FTS_PHYSICAL;
+    /* don't follow symlinks except those passed in command line*/
+    if (settings->samepart)
+        bit_flags|=FTS_XDEV;
+
+
+    while(settings->paths[cpindex] != NULL) {
+        /* The path points to a dir - recurse it! */
+        info("Now scanning "YEL"\"%s\""NCO" (%spreferred path)...",settings->paths[cpindex],
+             settings->is_ppath[cpindex] ? "" : "non-");
+        numfiles += traverse_path (settings, cpindex, bit_flags);
+        info(" done: %d files added.\n", numfiles);
+
+        cpindex++;
+    }
+
+
+    /* TODO: do we need to free up any memory? */
+    return (numfiles);
+}
