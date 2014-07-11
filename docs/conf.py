ab4731fb9947648162e21117223bf05e459b7357
Chris P <sahib@online.de>
2014-07-11 16:17:32 +0200

    Removed missing version var

--- docs/conf.py
+++ docs/conf.py
@@ -157,7 +157,7 @@ html_theme_path = ['_themes']
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
-html_title = 'rmlint ({version}) apidoc'.format(version=version)
+html_title = 'rmlint (2.x.x) apidoc'
 
 # A shorter title for the navigation bar.  Default is the same as html_title.
 #html_short_title = None
