e2a743392ee20fab87eef739a5c85bb18fa1f494
Chris P <sahib@online.de>
2014-11-30 23:37:50 +0100

    docs: still trying to feed css to rtd #2 screwed up.

--- docs/conf.py
+++ docs/conf.py
@@ -15,6 +15,11 @@
 import sys
 import os
 
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+sys.path.insert(0, os.path.abspath('./'))
+
 import sphinx_bootstrap_theme
 
 # Activate the theme.
