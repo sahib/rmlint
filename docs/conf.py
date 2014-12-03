50d7370cba3ef10ec6e1988f33e3e70fa0540f30
Christopher Pahl <sahib@online.de>
2014-12-03 13:05:50 +0100

    docs: oops, missing quote

--- docs/conf.py
+++ docs/conf.py
@@ -98,7 +98,7 @@ exclude_patterns = ['_build', '_themes/armstrong/README.rst']
 show_authors = True
 
 # The name of the Pygments (syntax highlighting) style to use.
-pygments_style = "default
+pygments_style = "default"
 
 # A list of ignored prefixes for module index sorting.
 #modindex_common_prefix = []
