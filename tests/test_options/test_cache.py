b715b79150af290365fc1733c7fc9f2b54ff3533
Christopher Pahl <sahib@online.de>
2015-01-09 01:43:15 +0100

    tests: added basic caching test

---
+++ tests/test_options/test_cache.py
@@ -0,0 +1,50 @@
+from nose import with_setup
+from tests.utils import *
+
+@with_setup(usual_setup_func, usual_teardown_func)
+def test_negative():
+    # Same size, different content.
+    create_file('a', '1.a')
+    create_file('b', '1.b')
+
+    # Same size, same content.
+    create_file('a', '2.a')
+    create_file('a', '2.a_')
+
+    # Different content and size
+    create_file('a' * 3, '3.a')
+    create_file('a' * 4, '3.a')
+
+    # duplicate_dirs + with --write_unfinished
+    create_file('x', 'dir_a/1')
+    create_file('x', 'dir_b/1')
+
+    for write_cache in True, False:
+        cache_path = os.path.join(TESTDIR_NAME, 'cache.json')
+
+        if write_cache:
+            head, *data, footer = run_rmlint('-U -D -S p -o json:{c}'.format(c=cache_path))
+        else:
+            head, *data, footer = run_rmlint('-D -S p -C {c}'.format(c=cache_path))
+
+        unfinished = [p['path'] for p in data if p['type'] == 'unfinished_cksum']
+        dupe_files = [p['path'] for p in data if p['type'] == 'duplicate_file']
+        dupe_trees = [p['path'] for p in data if p['type'] == 'duplicate_dir']
+
+        path_in = lambda name, paths: os.path.join(TESTDIR_NAME, name) in paths
+
+        if write_cache:
+            assert len(unfinished) == 3
+            assert path_in('1.b', unfinished)
+            assert path_in('dir_a/1', unfinished)
+            assert path_in('dir_b/1', unfinished)
+
+        assert len(dupe_trees) == 2
+        assert path_in('dir_a', dupe_trees)
+        assert path_in('dir_b', dupe_trees)
+
+        assert len(dupe_files) == 3
+        assert path_in('2.a', dupe_files)
+        assert path_in('2.a_', dupe_files)
+        assert path_in('1.a', dupe_files)
+
