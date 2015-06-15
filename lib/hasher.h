71799a92e008995512b8203a601ba509c9b32774
SeeSpotRun <thomas_d_j@yahoo.com.au>
2015-06-15 21:44:58 +1000

    hasher.h: make API more generic and less like a transplant from shredder

--- lib/hasher.h
+++ lib/hasher.h
@@ -29,10 +29,70 @@
 #include <glib.h>
 #include "config.h"
 
+/**
+ * @file hasher.h
+ * @brief High level API for file hashing
+ *
+ * Provides a high-level API for rmlint's low-level checksum.h API.
+ *
+ * File reading is done in the foreground thread (multi-threading of
+ * file reading is generally a bad idea because it tends to lead to
+ * disk thrash for rotational disks, and gives little or no benefit
+ * for non-rotational devices.
+ *
+ * File checksum calculation is done in one or more background threads,
+ * so that file reading can proceed uninterrupted.  This should give
+ * faster performance than conventional (single-threaded) checksum
+ * calculating utilities
+ *
+ *
+ **/
+
+
+/**
+ * @struct RmHasher
+ *
+ * RmHasher is an opaque data structure to represent a hashing engine.
+ * It should be created and destroyed via rm_hasher_new() and rm_hasher_free().
+ * File hashing tasks are carried out via RmHasherTask objects.
+ **/
 typedef struct _RmHasher RmHasher;
 
+/**
+ * @struct RmHasherTask
+ * RmHasherTask is an opaque data structure for carrying out file hashing.
+ * Individual tasks are created, executed and finalised via rm_hasher_task_new(),
+ * rm_hasher_task_hash() and rm_hasher_task_finish().
+ **/
 typedef _RmHasherTask RmHasherTask;
 
+/**
+ * @brief RmHasherCallback function prototype for rm_hasher_task_finish()
+ *
+ * @param digest The finished RmDigest
+ * @param session_user_data User data passed to rm_hasher_new()
+ * @param file_user_data User data passed to rm_hasher_task_new()
+ * @retval (UNUSED)
+ **/
+typedef int (*RmDigestCallback)(RmDigest *digest, gpointer session_user_data, gpointer task_user_data);
+
+
+/**
+ * @brief Allocate and initialise a new hashing object
+ *
+ * @param digest_type The type of digest
+ * @param num_threads The maximum number of hashing threads
+ * @param use_buffered_read If TRUE, read using preadv()
+ * @param buf_size Size of each read buffer size in bytes
+ * @param cache_quota_bytes Total bytes to allocate for read buffers
+ * @param target_kept_bytes Target number of bytes to be stored in paranoid digest buffers
+ * @param joiner The callback function for completed digests, triggered by rm_hasher_task_finish()
+ * If joiner is NULL then rm_hasher_task_finish() will wait for (background) hashing to finish
+ * and return the completed digest to the caller.
+ *
+ * @param session_user_data Pointer to user data associated with the session; will be passed to joiner.
+ *
+ **/
 RmHasher *rm_hasher_new(
             RmDigestType digest_type,
             uint num_threads,
@@ -40,12 +100,42 @@ RmHasher *rm_hasher_new(
             gsize buf_size,
             guint64 cache_quota_bytes,
             guint64 target_kept_bytes,
-            gpointer user_data);
+            RmDigestCallback joiner,
+            gpointer session_user_data);
 
+
+/**
+ * @brief Free a hashing object
+ **/
 void rm_hasher_free(RmHasher *hasher);
 
-RmHasherTask *rm_hasher_hash(RmHasher *hasher, char *path, RmDigest *digest, guint64 start_offset, guint64 bytes_to_read, gboolean is_symlink);
+/**
+ * @brief Allocate and initialise a new hashing task.
+ *
+ * @param hasher The hashing object
+ * @param digest An existing digest to update.  If NULL passed, will allocate a new digest.
+ * @param task_user_data Pointer to user data associated with the task; will be passed to session->joiner by rm_hasher_task_finish().
+ **/
+RmHasherTask *rm_hasher_task_new(RmHasher *hasher, RmDigest *digest, gpointer task_user_data);
 
-RmDigest *rm_hasher_finish_hash(RmHasherTask *task, RmDigestCallback callback, gpointer file_user_data);
+/**
+ * @brief Read data from a file and send it for hashing in separate thread
+ *
+ * @param task  An existing RmHasherTask
+ * @param path  The file path to read from
+ * @param start_offset  Where to start reading the file (number of bytes from start)
+ * @param bytes_to_read  How many bytes to read (pass 0 to read whole file)
+ * @param is_symlink  If path is a symlink, pass TRUE to read the symlink itself rather than the linked file
+ **/
+void rm_hasher_task_hash(RmHasherTask *task, char *path, guint64 start_offset, guint64 bytes_to_read, gboolean is_symlink);
+
+/**
+ * @brief Finalise a hashing task
+ *
+ * @param task  An existing RmHasherTask
+ *
+ * If a NULL callback passed, will wait for the task to finish and return the finalised RmDigest to caller.
+ **/
+RmDigest *rm_hasher_task_finish(RmHasherTask *task);
 
 #endif /* end of include guard */
