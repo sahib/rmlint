666933c538202b721dec81d6b68530a8f43e4d14
Christopher Pahl <sahib@online.de>
2015-08-03 01:54:02 +0200

    gui: partial cleanup of the long overdue settings view

--- gui/app/views/settings.py
+++ gui/app/views/settings.py
@@ -1,14 +1,32 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
+"""
+This is a rather generic interface for building a settings view
+from a GSettingsSchema, which is (usually) defined as XML File.
+
+For shredder this file is `org.gnome.Rmlint.gschema.xml` and contains a
+description of all keys used by Shredder.  Keys are typed in GSettings, so
+depending on the type a suitable widget is constructed.  Changes from outside
+are displayed by the widget and changes of the widget cause a change of the
+key's value.
+
+For reference, keys can be changed on the commandline too:
+$ gsettings --schemadir ~/.glib-schemas set org.gnome.Rmlint traverse-depth 2
+"""
+
 # Stdlib:
 import re
+import logging
+
+LOGGER = logging.getLogger('settings')
+
 from operator import itemgetter
 from functools import partial
 
 # Internal:
 from app.util import View, SuggestedButton, DestructiveButton
-from app.filesizerange import FileSizeRange
+from app.widgets import FileSizeRange, MultipleChoiceButton
 
 # External:
 from gi.repository import Gtk
@@ -18,247 +36,64 @@ from gi.repository import Gio
 from gi.repository import GObject
 
 
+################
+# TYPE WIDGETS #
+################
+
+
 def boolean_widget(settings, key_name, summary, description):
+    """Provide a simple widget for a boolean option key"""
     switch = Gtk.Switch()
     settings.bind(key_name, switch, 'active', 0)
     switch.set_active(settings.get_boolean(key_name))
     return switch
 
 
-def _format_range_value(spin, draw_size, draw_percent):
-    adj = spin.get_adjustment()
-    value = adj.get_value()
-
-    if draw_size:
-        spin.set_text('{}'.format(value))
-        return True
-    elif draw_percent:
-        spin.set_text('{}'.format(value * 100))
-        return True
-    return False
-
-
-def numeric_widget(
-    settings, key_name, summary, description,
-    floating_point=False, draw_percent=False, draw_size=True
-):
+def numeric_widget(settings, key_name, summary, desc, step=1):
+    """Provide a single widget to change a numeric key"""
+    # Use GSetting's "reflection" to get key metadata:
     schema = settings.get_property('settings-schema')
     key = schema.get_key(key_name)
     range_type, range_variant = key.get_range()
 
-    step = 0.1 if floating_point else 1
     range_wdgt = Gtk.SpinButton.new_with_range(0, 10 ** 10, step)
 
+    # Configure the range of the values if given:
     if range_type == 'range':
         min_val, max_val = range_variant
         range_wdgt.set_range(min_val, max_val)
 
     settings.bind(key_name, range_wdgt, 'value', 0)
-
-    if floating_point:
-        value = settings.get_double(key_name)
-    else:
-        value = settings.get_int(key_name)
-
-    range_wdgt.set_value(value)
-    # TODO: For weird reasons this is not working.
-    range_wdgt.connect('output', _format_range_value, draw_size, draw_percent)
+    func = settings.get_int if step is 1 else settings.get_double
+    range_wdgt.set_value(func(key_name))
     return range_wdgt
 
 
-class InteractiveLevelBar(Gtk.ProgressBar):
-    def __init__(self):
-        Gtk.ProgressBar.__init__(self)
-        self.set_can_focus(True)
-        self.grab_focus()
-
-        # Enable the receival of the appropiate signals:
-        self.add_events(self.get_events() |
-            Gdk.EventMask.BUTTON_PRESS_MASK |
-            Gdk.EventMask.BUTTON_RELEASE_MASK |
-            Gdk.EventMask.POINTER_MOTION_MASK |
-            Gdk.EventMask.SCROLL_MASK
-        )
-
-        self.set_size_request(100, 15)
-        self.connect(
-            'button-press-event',
-            InteractiveLevelBar._on_button_press
-        )
-
-    def set_value(self, value):
-        # Gtk.LevelBar.set_value(self, value)
-        self.set_fraction(value)
-
-    def _on_button_press(self, event):
-        if event.type == Gdk.EventType.BUTTON_RELEASE:
-            alloc = self.get_allocation()
-            percent = event.x / alloc.width
-            self.set_value(percent)
-            return True
-        return False
-
-
 def range_widget(settings, key_name, summary, description):
+    """Create a range widget for key types like (tt)"""
     min_val, max_val = settings.get_value(key_name)
     widget = FileSizeRange(min_val, max_val)
 
-    def _changed(*_):
+    # GSettings -> Widget
+    def setting_changed(*_):
         min_val, max_val = settings.get_value(key_name)
-        widget.props.min_value = min_val
-        widget.props.max_value = max_val
+        widget.min_value = min_val
+        widget.max_value = max_val
 
-    settings.connect('changed::' + key_name, _changed)
+    # Widget -> GSettings
+    def widget_changed(*_):
+        min_val, max_val = widget.min_value, widget.max_value
+        settings.set_value(key_name, GLib.Variant('(tt)', (min_val, max_val)))
+
+    # Bind manually:
+    settings.connect('changed::' + key_name, setting_changed)
+    widget.connect('value-changed', widget_changed)
 
     return widget
 
 
-class _ChoiceRow(Gtk.ListBoxRow):
-    def __init__(self, value, is_default):
-        Gtk.ListBoxRow.__init__(self)
-
-        box = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL)
-
-        label = Gtk.Label(value.capitalize())
-        label.props.xalign = 0
-
-        self.value, self.is_default = value, is_default
-        self.set_can_focus(False)
-
-        self.set_margin_left(3)
-        self.set_margin_right(3)
-
-        self.symbol = Gtk.Image.new_from_gicon(
-            Gio.ThemedIcon(name='emblem-ok-symbolic'),
-            Gtk.IconSize.BUTTON
-        )
-        self.symbol.set_no_show_all(True)
-
-        box.pack_start(label, True, True, 0)
-        box.pack_start(self.symbol, False, False, 0)
-        self.add(box)
-
-    def set_show_checkmark(self, state):
-        self.symbol.set_visible(state or self.is_default)
-
-        if self.is_default and not state:
-            icon_name, dim_down = 'non-starred-symbolic', True
-        elif self.is_default and state:
-            icon_name, dim_down = 'starred-symbolic', False
-        elif not self.is_default and state:
-            icon_name, dim_down = 'emblem-ok-symbolic', False
-        else:
-            icon_name, dim_down = None, False
-
-        if icon_name is not None:
-            self.symbol.set_from_gicon(
-                Gio.ThemedIcon(name=icon_name), Gtk.IconSize.BUTTON
-            )
-
-        ctx = self.symbol.get_style_context()
-        if dim_down:
-            ctx.add_class(Gtk.STYLE_CLASS_DIM_LABEL)
-            self.symbol.set_opacity(0.5)
-        else:
-            ctx.remove_class(Gtk.STYLE_CLASS_DIM_LABEL)
-            self.symbol.set_opacity(1.0)
-
-
-class _CurrentChoiceLabel(Gtk.Label):
-    def __init__(self, text):
-        Gtk.Label.__init__(self)
-
-        self.set_use_markup(True)
-        self.pretty_text = text
-
-    @GObject.Property(type=str, default='')
-    def choice(self):
-        self._choice
-
-    @choice.setter
-    def set_choice(self, new_value):
-        self._choice = new_value
-        self.set_markup(
-            '<u>{v}</u>'.format(
-                v=self._choice.capitalize()
-            )
-        )
-
-        self.notify('choice')
-
-
-class MultipleChoiceButton(Gtk.Button):
-    __gsignals__ = {
-        'row-selected': (GObject.SIGNAL_RUN_FIRST, None, ()),
-    }
-
-    def __init__(self, values, default, selected, summary):
-        Gtk.Button.__init__(self)
-
-        self._selected_row = None
-
-        self.set_relief(Gtk.ReliefStyle.NONE)
-        self.set_can_focus(False)
-
-        self.value_label = _CurrentChoiceLabel(default)
-        self.add(self.value_label)
-
-        popover = Gtk.Popover.new(self)
-        popover.set_modal(True)
-        self.connect('clicked', lambda *_: popover.show_all())
-
-        listbox = Gtk.ListBox()
-        listbox.set_border_width(10)
-        listbox.set_selection_mode(Gtk.SelectionMode.NONE)
-        listbox.set_activate_on_single_click(True)
-
-        listbox.connect('row-activated', self.on_update_value, popover)
-        listbox_header = Gtk.Label(
-            '<small><b>{txt}?</b></small>'.format(txt=summary)
-        )
-        listbox_header.get_style_context().add_class(
-            Gtk.STYLE_CLASS_DIM_LABEL
-        )
-        listbox_header.set_use_markup(True)
-        listbox_header.set_size_request(90, -1)
-
-        for widget in Gtk.Separator(), listbox_header:
-            row = Gtk.ListBoxRow()
-            row.set_selectable(False)
-            row.set_activatable(False)
-            row.add(widget)
-            listbox.prepend(row)
-
-        frame = Gtk.Frame()
-        frame.add(listbox)
-        frame.set_border_width(5)
-        popover.add(frame)
-
-        for choice in values:
-            row = _ChoiceRow(choice, default == choice)
-            listbox.add(row)
-
-            if choice == selected:
-                listbox.select_row(row)
-                row.set_show_checkmark(True)
-
-        self.connect('clicked', lambda *_: popover.show_all())
-
-    def get_selected_row(self):
-        return self._selected_row
-
-    def on_update_value(self, listbox, row, popover):
-        for other_row in listbox:
-            # Might be a different type:
-            if isinstance(other_row, _ChoiceRow):
-                other_row.set_show_checkmark(row is other_row)
-
-        self._selected_row = row
-        self.emit('row-selected')
-        popover.hide()
-
-
 def choice_widget(settings, key_name, summary, description):
+    """Create a widget for choosing between a list of selections"""
     schema = settings.props.settings_schema
     key = schema.get_key(key_name)
 
@@ -267,28 +102,33 @@ def choice_widget(settings, key_name, summary, description):
 
     range_type, range_variant = key.get_range()
     if range_type != 'enum':
-        print('uh oh')
+        LOGGER.warning('{k} is not an enum.'.format(k=key_name))
+        return
 
     choices = list(range_variant)
     button = MultipleChoiceButton(choices, default, selected, summary)
     button.connect(
         'row-selected',
-        lambda _: settings.set_string(key_name, button.get_selected_row().value)
+        lambda _: settings.set_string(key_name, button.get_selected_choice())
     )
 
     settings.bind(key_name, button.value_label, 'choice', 0)
-
     return button
 
 
 VARIANT_TO_WIDGET = {
     'b': boolean_widget,
-    'i': partial(numeric_widget, floating_point=False),
-    'd': partial(numeric_widget, floating_point=True),
+    'i': partial(numeric_widget),
+    'd': partial(numeric_widget, step=0.1),
     's': choice_widget,
     '(ii)': range_widget,
     '(tt)': range_widget
 }
+
+
+#####################
+#    ACTUAL VIEW    #
+#####################
 
 
 class SettingsView(View):
@@ -317,9 +157,11 @@ class SettingsView(View):
         self.appy_btn.connect('clicked', self.on_apply_settings)
         self.deny_btn.connect('clicked', self.on_reset_to_defaults)
 
-        self.fill_from_settings()
+        # Initialize from current settings:
+        self.build()
 
     def append_section(self, heading):
+        """Append a new named section for multiple entries with `heading`"""
         box = Gtk.ListBox()
         box.set_selection_mode(Gtk.SelectionMode.NONE)
         box.set_size_request(350, -1)
@@ -342,6 +184,14 @@ class SettingsView(View):
         self._grid.attach(frame, 0, len(self._grid), 1, 1)
 
     def append_entry(self, section, key_name, val_widget, summary, desc=None):
+        """Append an entry to a named section.
+
+        section: A prviously inserted section name.
+        key_name: The key to bind.
+        val_widget: The widget to show the key in.
+        summary: A short summary to show.
+        desc: A longer description.
+        """
         desc_label = Gtk.Label(desc or '')
         summ_label = Gtk.Label(summary or '')
 
@@ -364,8 +214,7 @@ class SettingsView(View):
         sub_grid.set_border_width(3)
 
         listbox = self.sections[section.lower()]
-
-        if len(listbox) is not 0:
+        if len(listbox):
             sep_row = Gtk.ListBoxRow()
             sep_row.set_activatable(False)
             sep_row.add(Gtk.Separator())
@@ -377,10 +226,12 @@ class SettingsView(View):
         listbox.insert(row, -1)
 
     def reset_to_defaults(self):
+        """Reset whole view and keys to their defaults"""
         for key_name in self.app.settings.list_keys():
             self.app.settings.reset(key_name)
 
-    def fill_from_settings(self):
+    def build(self):
+        """Built all entries and sections"""
         gst = self.app.settings
         schema = gst.get_property('settings-schema')
 
@@ -430,6 +281,10 @@ class SettingsView(View):
 
         for entry in sorted(entry_rows, key=itemgetter(0, 2)):
             self.append_entry(*entry[1:])
+
+    ####################
+    # SIGNAL CALLBACKS #
+    ####################
 
     def on_view_enter(self):
         self.save_settings = False
