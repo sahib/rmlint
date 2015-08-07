6e3c44057b491e90d2c801c00f25d2b96263d112
Christopher Pahl <sahib@online.de>
2015-08-07 18:17:20 +0200

    gui: pylint: application.py

--- gui/shredder/application.py
+++ gui/shredder/application.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
+"""Shredder's GtkApplication implementation.
+
+It loads all initially required resources and triggers
+the gui build by instancing the MainWindow.
+"""
+
 # Stdlib:
 import os
 import gettext
@@ -24,6 +30,7 @@ from gi.repository import Gtk, Gio, Rsvg, GdkPixbuf
 
 
 def _create_action(name, callback=None):
+    """Create a named GAction with a callback for it's activation"""
     action = Gio.SimpleAction.new(name, None)
     if callback is not None:
         action.connect('activate', callback)
@@ -32,6 +39,7 @@ def _create_action(name, callback=None):
 
 
 def _load_app_icon():
+    """Load & render the application svg icon from the resource bundle"""
     logo_svg = Gio.resources_lookup_data('/org/gnome/shredder/shredder.svg', 0)
     logo_handle = Rsvg.Handle.new_from_data(logo_svg.get_data())
     logo_handle.set_dpi_x_y(75, 75)
@@ -41,18 +49,21 @@ def _load_app_icon():
 
 
 class Application(Gtk.Application):
+    """GtkApplication implementation of Shredder."""
     def __init__(self):
         Gtk.Application.__init__(
             self,
             application_id='org.gnome.Shredder',
             flags=Gio.ApplicationFlags.FLAGS_NONE
         )
+        self.settings = self.win = None
 
-    def do_activate(self):
+    def do_activate(self, **kw):
+        Gtk.Application.do_activate(self, **kw)
         self.win.present()
 
-    def do_startup(self):
-        Gtk.Application.do_startup(self)
+    def do_startup(self, **kw):
+        Gtk.Application.do_startup(self, **kw)
 
         # Make tranlsating strings possible:
         gettext.install(APP_TITLE)
@@ -64,7 +75,9 @@ class Application(Gtk.Application):
         Gio.resources_register(resource_bundle)
 
         # Load the application CSS files.
-        css_data = Gio.resources_lookup_data('/org/gnome/shredder/shredder.css', 0)
+        css_data = Gio.resources_lookup_data(
+            '/org/gnome/shredder/shredder.css', 0
+        )
         load_css_from_data(css_data.get_data())
 
         # Init the config system
