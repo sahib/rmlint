85f1dc639b0b46a1ab541835135b0335535f2d71
Christopher Pahl <sahib@online.de>
2015-09-29 00:43:44 +0200

    doc: add --unmatched-basename (previously --no-match-basename)

--- tests/test_options/test_match_basenames.py
+++ tests/test_options/test_match_basenames.py
@@ -2,7 +2,7 @@ from nose import with_setup
 from tests.utils import *
 
 @with_setup(usual_setup_func, usual_teardown_func)
-def test_negative():
+def test_negative_with_basename():
     create_file('xxx', 'a')
     create_file('xxx', 'b')
     head, *data, footer = run_rmlint('-b')
@@ -12,10 +12,30 @@ def test_negative():
 
 
 @with_setup(usual_setup_func, usual_teardown_func)
-def test_positive():
+def test_positive_with_basename():
     create_file('xxx', 'a/test')
     create_file('xxx', 'b/test')
     head, *data, footer = run_rmlint('-b')
+    assert footer['total_files'] == 2
+    assert footer['total_lint_size'] == 3
+    assert footer['duplicates'] == 1
+
+
+@with_setup(usual_setup_func, usual_teardown_func)
+def test_negative_without_basename():
+    create_file('xxx', 'a/test')
+    create_file('xxx', 'b/test')
+    head, *data, footer = run_rmlint('-B')
+    assert footer['total_files'] == 2
+    assert footer['total_lint_size'] == 0
+    assert footer['duplicates'] == 0
+
+
+@with_setup(usual_setup_func, usual_teardown_func)
+def test_positive_without_basename():
+    create_file('xxx', 'a/test1')
+    create_file('xxx', 'b/test2')
+    head, *data, footer = run_rmlint('-B')
     assert footer['total_files'] == 2
     assert footer['total_lint_size'] == 3
     assert footer['duplicates'] == 1
