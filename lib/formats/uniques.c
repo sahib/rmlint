8d4ed659c1d7ddfe42f44dbbcb765f9a7287c8f0
Christopher Pahl <sahib@online.de>
2015-11-03 14:40:47 +0100

    unique/fdupes: Remove caching from those formatters.

--- lib/formats/uniques.c
+++ lib/formats/uniques.c
@@ -32,19 +32,10 @@
 typedef struct RmFmtHandlerUniques {
     /* must be first */
     RmFmtHandler parent;
-
-    /* Storage for all strings */
-    GStringChunk *text_chunks;
-
-    /* Pointers into text_chunks */
-    GQueue *text_lines;
-
 } RmFmtHandlerUniques;
 
 static void rm_fmt_elem(_U RmSession *session, _U RmFmtHandler *parent, _U FILE *out,
                         RmFile *file) {
-    RmFmtHandlerUniques *self = (RmFmtHandlerUniques *)parent;
-
     if(file->lint_type != RM_LINT_TYPE_UNIQUE_FILE) {
         /* we only not want to list unfinished files. */
         return;
@@ -58,45 +49,9 @@ static void rm_fmt_elem(_U RmSession *session, _U RmFmtHandler *parent, _U FILE 
         return;
     }
 
-    char line[512 + 32];
-    memset(line, 0, sizeof(line));
     RM_DEFINE_PATH(file);
-
-    g_snprintf(line, sizeof(line), "%s\n", file_path);
-
-    if(self->text_chunks == NULL) {
-        self->text_chunks = g_string_chunk_new(PATH_MAX / 2);
-        self->text_lines = g_queue_new();
-    }
-
-    /* remember the line (use GStringChunk for effiecient storage) */
-    g_queue_push_tail(self->text_lines, g_string_chunk_insert(self->text_chunks, line));
-}
-
-static void rm_fmt_prog(RmSession *session,
-                        _U RmFmtHandler *parent,
-                        FILE *out,
-                        RmFmtProgressState state) {
-    RmFmtHandlerUniques *self = (RmFmtHandlerUniques *)parent;
-
-    extern RmFmtHandler *PROGRESS_HANDLER;
-    rm_assert_gentle(PROGRESS_HANDLER->prog);
-    PROGRESS_HANDLER->prog(session, (RmFmtHandler *)PROGRESS_HANDLER, stderr, state);
-
-    /* Print all cached lines on shutdown. */
-    if(state == RM_PROGRESS_STATE_PRE_SHUTDOWN && self->text_lines) {
-        fprintf(out, "%sUNIQUE FILES:%s\n", MAYBE_GREEN(out, session), MAYBE_RESET(out, session));
-        //fprintf(out, "UNIQUE FILES:\n");
-        for(GList *iter = self->text_lines->head; iter; iter = iter->next) {
-            char *line = iter->data;
-            if(line != NULL) {
-                fprintf(out, "%s", line);
-            }
-        }
-        g_queue_free(self->text_lines);
-        g_string_chunk_free(self->text_chunks);
-        fprintf(out, "\n");
-    }
+    fputs(file_path, out);
+    fputc('\n', out);
 }
 
 static RmFmtHandlerUniques UNIQUES_HANDLER_IMPL = {
@@ -107,12 +62,10 @@ static RmFmtHandlerUniques UNIQUES_HANDLER_IMPL = {
             .name = "uniques",
             .head = NULL,
             .elem = rm_fmt_elem,
-            .prog = rm_fmt_prog,
+            .prog = NULL,
             .foot = NULL,
             .valid_keys = {NULL},
-        },
-    .text_lines = NULL,
-
+        }
 };
 
 RmFmtHandler *UNIQUES_HANDLER = (RmFmtHandler *)&UNIQUES_HANDLER_IMPL;
