58e0dd8a84735efb0ee7ac103d523cbc5fbfccb1
SeeSpotRun <thomas_d_j@yahoo.com.au>
2017-04-06 22:56:40 +1000

    preprocess: consider other criteria in rm_file_cmp_full() if mtimes equal

--- lib/preprocess.c
+++ lib/preprocess.c
@@ -97,11 +97,9 @@ static gint rm_file_cmp_full(const RmFile *file_a, const RmFile *file_b,
 
     if(session->cfg->mtime_window >= 0) {
         gdouble diff = file_a->mtime - file_b->mtime;
-        if(FLOAT_IS_ZERO(diff)) {
-            return 0;
+        if(!(FLOAT_IS_ZERO(diff))) {
+            return (diff < 0) ? -1 : +1;
         }
-
-        return (diff < 0) ? -1 : +1;
     }
 
     return rm_pp_cmp_orig_criteria(file_a, file_b, session);
