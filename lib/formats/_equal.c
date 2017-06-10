dd5bfd7048cc27d391c489d3d344221447f3e837
Chris P <sahib@online.de>
2017-06-10 17:22:30 +0200

    --equal: Optimize for large number of input files (e.g. when piping to stdin)

--- lib/formats/_equal.c
+++ lib/formats/_equal.c
@@ -35,84 +35,92 @@ typedef struct RmFmtHandlerEqual {
     /* must be first */
     RmFmtHandler parent;
 
-	/* Checksum of the last checked file (or NULL) */
-	char *last_checksum;
+    /* Checksum of the last checked file (or NULL) */
+    char *last_checksum;
 
-	/* Set to true once a mismatch (colliding cksum) was found */
-	bool mismatch_found;
+    /* Set to true once a mismatch (colliding cksum) was found */
+    bool mismatch_found;
+
+    /* session->cfg->paths turned to a set for efficient member test */
+    GHashTable *input_paths;
 } RmFmtHandlerEqual;
 
 /////////////////////////
 //  ACTUAL CALLBACKS   //
 /////////////////////////
 
+static void rm_fmt_head(RmSession *session, RmFmtHandler *parent, _UNUSED FILE *out) {
+    GHashTable *input_paths = g_hash_table_new(g_str_hash, g_str_equal);
+
+    GSList *paths = session->cfg->paths;
+    for(GSList *iter = paths; iter; iter = iter->next) {
+        RmPath *path = iter->data;
+        g_hash_table_add(input_paths, path->path);
+    }
+
+    RmFmtHandlerEqual *self = (RmFmtHandlerEqual *)parent;
+    self->input_paths = input_paths;
+}
+
 static void rm_fmt_elem(
     RmSession *session,
     RmFmtHandler *self_ref,
     _UNUSED FILE *out,
-	RmFile *file
+    RmFile *file
 ) {
     RmFmtHandlerEqual *self = (RmFmtHandlerEqual *)self_ref;
 
-	/*  No need to check anymore, it's not equal. */
-	if(self->mismatch_found) {
-		return;
-	}
+    /*  No need to check anymore, it's not equal. */
+    if(self->mismatch_found) {
+        return;
+    }
 
-	if(file->digest == NULL) {
-		/* We do not want to handle unique files here.
-		 * If it is unique, it will be not equal...
-		 * */
-		self->mismatch_found = true;
-		return;
-	}
+    if(file->digest == NULL) {
+        /* We do not want to handle unique files here.
+         * If it is unique, it will be not equal...
+         * */
+        self->mismatch_found = true;
+        return;
+    }
 
-	RM_DEFINE_PATH(file);
+    RM_DEFINE_PATH(file);
 
-	bool path_explictly_given = false;
-	GSList *paths = session->cfg->paths;
-	for(GSList *iter = paths; iter; iter = iter->next) {
-		RmPath *path = iter->data;
-		if(!strcmp(path->path, file_path)) {
-			path_explictly_given = true;
-		}
-	}
+    if(g_hash_table_contains(self->input_paths, file_path) == false) {
+        /* Ignore; this path was not given explicitly on the cmdline */
+        return;
+    }
 
-	if(path_explictly_given == false) {
-		/* Ignore, this path was not given explicitly. */
-		return;
-	}
-
-	size_t cksum_bytes = rm_digest_get_bytes(file->digest) * 2 + 1;
-	char *checksum = g_malloc0(cksum_bytes);
+    size_t cksum_bytes = rm_digest_get_bytes(file->digest) * 2 + 1;
+    char *checksum = g_malloc0(cksum_bytes);
 
     memset(checksum, '0', cksum_bytes);
     checksum[cksum_bytes - 1] = 0;
     rm_digest_hexstring(file->digest, checksum);
 
-	if(self->last_checksum != NULL) {
-		if(!strncmp(checksum, self->last_checksum, cksum_bytes)) {
-			session->equal_exit_code = EXIT_SUCCESS;
-		} else {
-			session->equal_exit_code = EXIT_FAILURE;
-			self->mismatch_found = true;
-			rm_log_debug_line(
-					"First differing items:\n\t%s (%s)\n\tlast checksum: (%s)",
-					file_path, checksum, self->last_checksum
-			);
-		}
-		g_free(self->last_checksum);
-	}
+    if(self->last_checksum != NULL) {
+        if(!strncmp(checksum, self->last_checksum, cksum_bytes)) {
+            session->equal_exit_code = EXIT_SUCCESS;
+        } else {
+            session->equal_exit_code = EXIT_FAILURE;
+            self->mismatch_found = true;
+            rm_log_debug_line(
+                    "First differing items:\n\t%s (%s)\n\tlast checksum: (%s)",
+                    file_path, checksum, self->last_checksum
+            );
+        }
+        g_free(self->last_checksum);
+    }
 
-	self->last_checksum = checksum;
+    self->last_checksum = checksum;
 }
 
 static void rm_fmt_foot(
-		_UNUSED RmSession *session,
-		RmFmtHandler *parent,
-		_UNUSED FILE *out) {
+        _UNUSED RmSession *session,
+        RmFmtHandler *parent,
+        _UNUSED FILE *out) {
     RmFmtHandlerEqual *self = (RmFmtHandlerEqual *)parent;
-	g_free(self->last_checksum);
+    g_hash_table_unref(self->input_paths);
+    g_free(self->last_checksum);
 }
 
 static RmFmtHandlerEqual EQUAL_HANDLE_IMPL = {
@@ -120,14 +128,14 @@ static RmFmtHandlerEqual EQUAL_HANDLE_IMPL = {
     .parent = {
         .size = sizeof(EQUAL_HANDLE_IMPL),
         .name = "_equal",
-        .head = NULL,
+        .head = rm_fmt_head,
         .elem = rm_fmt_elem,
         .prog = NULL,
         .foot = rm_fmt_foot,
         .valid_keys = {NULL},
     },
-	.last_checksum = NULL,
-	.mismatch_found = false
+    .last_checksum = NULL,
+    .mismatch_found = false
 };
 
 RmFmtHandler *EQUAL_HANDLER = (RmFmtHandler *) &EQUAL_HANDLE_IMPL;
