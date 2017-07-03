340ec056cdd6cd58884963e6a32f3945b440b841
SeeSpotRun <thomas_d_j@yahoo.com.au>
2017-07-03 10:25:05 +1000

    tests: add tests for rmlint --btrfs-clone

---
+++ tests/test_mains/test_clone.py
@@ -0,0 +1,132 @@
+#!/usr/bin/env python3
+# encoding: utf-8
+
+from nose import with_setup
+from nose.tools import make_decorator
+from nose.plugins.skip import SkipTest
+from contextlib import contextmanager
+import psutil
+
+from tests.utils import *
+
+
+@contextmanager
+def assert_exit_code(status_code):
+    """
+    Assert that the with block yields a subprocess.CalledProcessError
+    with a certain return code. If nothing is thrown, status_code
+    is required to be 0 to survive the test.
+    """
+    try:
+        yield
+    except subprocess.CalledProcessError as exc:
+        assert exc.returncode == status_code
+    else:
+        # No exception? status_code should be fine.
+        assert status_code == 0
+
+
+def is_btrfs(path):
+    parts = psutil.disk_partitions(all=True)
+
+    # iterate up from `path` until mountpoint found
+    p = path
+    while 1:
+        match = next((x for x in parts if x.mountpoint == p), None)
+        if (match):
+            print("{0} is {1} mounted at {2}".format(path, match.fstype, p))
+            return (match.fstype == 'btrfs')
+
+        if (p == '/'):
+            # probably should never get here...
+            print("no mountpoint found for {0}".format(path))
+            return False
+        p = os.path.dirname(p)
+
+
+# decorator for tests dependent on btrfs testdir
+def needs_btrfs(test):
+    def no_support(*args):
+        raise SkipTest("btrfs not supported")
+
+    def not_btrfs(*args):
+        raise SkipTest("testdir is not on btrfs filesystem")
+
+    if not has_feature('btrfs-support'):
+        return make_decorator(test)(no_support)
+    elif not is_btrfs(TESTDIR_NAME):
+        return make_decorator(test)(not_btrfs)
+    else:
+        return test
+
+
+@needs_btrfs
+@with_setup(usual_setup_func, usual_teardown_func)
+def test_equal_files():
+    path_a = create_file('1234', 'a')
+    path_b = create_file('1234', 'b')
+
+    with assert_exit_code(0):
+        head, *data, footer = run_rmlint(
+            '--btrfs-clone',
+            path_a, path_b,
+            use_default_dir=False,
+            with_json=False,
+            verbosity="")
+
+    with assert_exit_code(0):
+        head, *data, footer = run_rmlint(
+            '--btrfs-clone',
+            path_a, '//', path_b,
+            use_default_dir=False,
+            with_json=False)
+
+
+@needs_btrfs
+@with_setup(usual_setup_func, usual_teardown_func)
+def test_different_files():
+    path_a = create_file('1234', 'a')
+    path_b = create_file('4321', 'b')
+
+    with assert_exit_code(1):
+        head, *data, footer = run_rmlint(
+            '--btrfs-clone',
+            path_a, path_b,
+            use_default_dir=False,
+            with_json=False,
+            verbosity="")
+
+
+@needs_btrfs
+@with_setup(usual_setup_func, usual_teardown_func)
+def test_bad_arguments():
+    path_a = create_file('1234', 'a')
+    path_b = create_file('1234', 'b')
+    path_c = create_file('1234', 'c')
+    for paths in [
+            path_a,
+            ' '.join((path_a, path_b, path_c)),
+            ' '.join((path_a, path_a + ".nonexistent"))
+    ]:
+        with assert_exit_code(1):
+            head, *data, footer = run_rmlint(
+                '--btrfs-clone',
+                paths,
+                use_default_dir=False,
+                with_json=False,
+                verbosity="")
+
+
+@needs_btrfs
+@with_setup(usual_setup_func, usual_teardown_func)
+def test_directories():
+    path_a = os.path.dirname(create_dirs('dir_a'))
+    path_b = os.path.dirname(create_dirs('dir_b'))
+
+    with assert_exit_code(1):
+        head, *data, footer = run_rmlint(
+            '--btrfs-clone',
+            path_a, path_b,
+            use_default_dir=False,
+            with_json=False,
+            verbosity="")
--- tests/utils.py
+++ tests/utils.py
@@ -64,7 +64,14 @@ def has_feature(feature):
     ).decode('utf-8')
 
 
-def run_rmlint_once(*args, dir_suffix=None, use_default_dir=True, outputs=None, directly_return_output=False, use_shell=False):
+def run_rmlint_once(*args,
+                    dir_suffix=None,
+                    use_default_dir=True,
+                    outputs=None,
+                    with_json=True,
+                    directly_return_output=False,
+                    use_shell=False,
+                    verbosity="-V"):
     if use_default_dir:
         if dir_suffix:
             target_dir = os.path.join(TESTDIR_NAME, dir_suffix)
@@ -87,10 +94,11 @@ def run_rmlint_once(*args, dir_suffix=None, use_default_dir=True, outputs=None, 
         env, cmd = {}, []
 
     cmd += [
-        './rmlint', target_dir, '-V',
-    ] + shlex.split(' '.join(args)) + [
-        '-o', 'json:/tmp/out.json', '-c', 'json:oneline'
-    ]
+        './rmlint', target_dir, verbosity,
+    ] + shlex.split(' '.join(args))
+
+    if with_json:
+        cmd += ['-o', 'json:/tmp/out.json', '-c', 'json:oneline']
 
     for idx, output in enumerate(outputs or []):
         cmd.append('-o')
@@ -254,7 +262,14 @@ def run_rmlint(*args, force_no_pendantic=False, **kwargs):
 
 
 def create_dirs(path):
-    os.makedirs(os.path.join(TESTDIR_NAME, path))
+    full_path = os.path.join(TESTDIR_NAME, path)
+
+    try:
+        os.makedirs(full_path)
+    except OSError:
+        pass
+
+    return full_path
 
 
 def create_link(path, target, symlink=False):
