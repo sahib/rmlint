d913934b5f7a6df7ef98155ec0d5bb6010da2dd8
SeeSpotRun <thomas_d_j@yahoo.com.au>
2017-11-14 23:38:48 +1000

    xattr: fix logic error (thanks clang)

--- lib/xattr.c
+++ lib/xattr.c
@@ -199,7 +199,7 @@ gboolean rm_xattr_read_hash(RmFile *file, RmSession *session) {
         return FALSE;
     }
 
-    if(cksum_hex_str == NULL || strcmp(cksum_hex_str, "")==0) {
+    if(cksum_hex_str[0] == 0 || strcmp(cksum_hex_str, "")==0) {
         return FALSE;
     }
 
