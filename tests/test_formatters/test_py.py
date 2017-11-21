128233c1fb8b88cbb9d24abf31eb3d02e2657dd7
Chris Pahl <sahib@online.de>
2017-11-21 01:40:03 +0100

    py: add very basic test coverage for different python versions

--- tests/test_formatters/test_py.py
+++ tests/test_formatters/test_py.py
@@ -5,17 +5,37 @@ from tests.utils import *
 
 import subprocess
 
+from parameterized import parameterized
 
+
+def _check_interpreter(interpreter):
+    try:
+        subprocess.call([interpreter, "-c", "1 + 1"])
+        return True
+    except (subprocess.CalledProcessError, FileNotFoundError):
+        return False
+
+
+@parameterized(["python2", "python3"])
 @with_setup(usual_setup_func, usual_teardown_func)
-def test_paranoia():
+def test_paranoia(interpreter):
+    if not _check_interpreter(interpreter):
+        print(
+            "Interpreter {} does not seem to be working, skipping test".format(
+                interpreter
+            )
+        )
+        return
+
     create_file('xxx', 'a')
     create_file('xxx', 'b')
     create_file('xxx', 'c')
     create_file('xxx', 'd')
     create_link('a', 'hardlink_a', symlink=False)
 
-    head, *data, footer = run_rmlint('-S a -o py:{t}/rmlint.py'.format(t=TESTDIR_NAME))
-    # subprocess.call('cat ' + os.path.join(TESTDIR_NAME, 'rmlint.py'), shell=True)
+    head, *data, footer = run_rmlint(
+        '-S a -o py:{t}/rmlint.py'.format(t=TESTDIR_NAME)
+    )
 
     assert footer['duplicate_sets'] == 1
     assert footer['total_lint_size'] == 9
@@ -28,11 +48,18 @@ def test_paranoia():
     with open(os.path.join(TESTDIR_NAME, 'c'), 'w') as handle:
         handle.write('xxxx')
 
-    text = subprocess.check_output([os.path.join(TESTDIR_NAME, 'rmlint.py'), '-d', '-p'])
+    text = subprocess.check_output([
+        interpreter,
+        os.path.join(TESTDIR_NAME, 'rmlint.py'),
+        '-d',
+        '-p'
+    ])
     text = text.decode('utf-8')
 
     # subprocess.call('ls  -l ' + TESTDIR_NAME, shell=True)
-    head, *data, footer = run_rmlint('-S a -o py:{t}/rmlint.py'.format(t=TESTDIR_NAME))
+    head, *data, footer = run_rmlint(
+        '-S a -o py:{t}/rmlint.py'.format(t=TESTDIR_NAME)
+    )
 
     assert footer['duplicate_sets'] == 1
     assert footer['total_lint_size'] == 0
@@ -43,8 +70,15 @@ def test_paranoia():
     assert 'Size differs' in text
     assert 'Same inode' in text
 
-    text = subprocess.check_output([os.path.join(TESTDIR_NAME, 'rmlint.py'), '-d', '-p'])
-    head, *data, footer = run_rmlint('-S a -o py:{t}/rmlint.py'.format(t=TESTDIR_NAME))
+    text = subprocess.check_output([
+        interpreter,
+        os.path.join(TESTDIR_NAME, 'rmlint.py'),
+        '-d',
+        '-p'
+    ])
+    head, *data, footer = run_rmlint(
+        '-S a -o py:{t}/rmlint.py'.format(t=TESTDIR_NAME)
+    )
 
     # Nothing should change.
     assert footer['duplicate_sets'] == 1
