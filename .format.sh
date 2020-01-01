d442a4947001d771df332a0386a920ae91183704
Ryan Delaney <ryan.delaney@gmail.com>
2019-12-31 17:52:04 -0800

    Revert adding quotes around ${FILES}

--- .format.sh
+++ .format.sh
@@ -3,11 +3,11 @@
 FILES=$(find src lib -iname '*.[ch]')
 
 # Format the files according to the style defined in .clang-format
-clang-format -i -style=file "${FILES}" || (echo 'clang-format failed'; exit 1);
+clang-format -i -style=file ${FILES} || (echo 'clang-format failed'; exit 1);
 
 # clang-format sometimes adds windows like line-endins (\r\n).
 # Fix this by issueing dos2unix on every file.
-dos2unix --quiet "${FILES}" || (echo 'dos2unix failed'; exit 2);
+dos2unix --quiet ${FILES} || (echo 'dos2unix failed'; exit 2);
 
 # Some bad editors might save files with a bad encoding.
 # Since a few source code files contain unicode, we fix this with iconv.
