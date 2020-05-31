c5358c6015e7042c35944334b73b0e88ef68cee3
Chris Pahl <sahib@online.de>
2020-05-31 18:17:32 +0200

    fix broken test_merge_directories_with_ignored_symlinks

--- tests/test_options/test_symlinks.py
+++ tests/test_options/test_symlinks.py
@@ -31,7 +31,7 @@ def test_merge_directories_with_ignored_symlinks():
     create_link('bogus', 'b/link', symlink=True)
 
     head, *data, footer = run_rmlint('-T df,dd')
-    assert {e["path"][len(TESTDIR_NAME):] for e in data} == {
+    assert {e["path"][len(TESTDIR_NAME):] for e in data if e["type"] == "duplicate_dir"} == {
         '/a',
         '/b',
     }
