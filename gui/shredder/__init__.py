a041abb37a995d347cfd8618209ae8689efc7eee
Vassili Tchersky <vt+git@vbcy.org>
2026-07-21 12:48:05 +0000

    gui: pin GdkPixbuf 2.0 to remove PyGI warning

--- gui/shredder/__init__.py
+++ gui/shredder/__init__.py
@@ -3,6 +3,7 @@
 import gi
 
 gi.require_version('Gtk', '3.0')
+gi.require_version('GdkPixbuf', '2.0')
 gi.require_version('Rsvg', '2.0')
 gi.require_version('PangoCairo', '1.0')
 gi.require_version('GtkSource', '4')
@@ -21,10 +22,8 @@ APP_USE_TRADITIONAL_MENU = False
 
 def run_gui():
     """Fully take over and run the gui code."""
-    # Stdlib:
     import sys
 
-    # Internal:
     from shredder.cmdline import parse_arguments
     from shredder.logger import create_logger
 
